73bbafecd1b6e7ce9ed3b3b400cf10529ceee014
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-12-04 10:06:40 +0100

    Add autojump to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -19,3 +19,11 @@ function fish_prompt
         printf '%s ' (__fish_git_prompt)
        set_color normal
 end
+
+# add autojump
+begin
+    set --local AUTOJUMP_PATH $HOME/.autojump/share/autojump/autojump.fish
+    if test -e $AUTOJUMP_PATH
+        source $AUTOJUMP_PATH
+    end
+end
