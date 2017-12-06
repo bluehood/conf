a599839b4e4285e48957092d6fc105507025f006
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-12-06 11:28:54 +0100

    Improve cern762 fish function

--- fish/functions/cern762.fish
+++ fish/functions/cern762.fish
@@ -1,8 +1,12 @@
 # Defined in - @ line 2
 function cern762
-	if host cern762 >/dev/null
-        ssh -t eguiraud@cern762 tmux attach
+	set opt "attach"
+    if [ -n "$argv[1]" -a "$argv[1]" = "new" ]
+        set opt "new"
+    end
+    if host cern762 >/dev/null
+        ssh -t eguiraud@cern762 tmux $opt
     else
-        ssh -t eguiraud@lxplus.cern.ch -- ssh -t eguiraud@cern762 tmux attach
+        ssh -t eguiraud@lxplus.cern.ch -- ssh -t eguiraud@cern762 tmux $opt
     end
 end
