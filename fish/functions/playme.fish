23d26b472568feb827bfe2a09a9b4bca94ce999b
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-12-24 20:25:35 +0100

    More robust `playme` command, always started in i3 scratchpad

--- fish/functions/playme.fish
+++ fish/functions/playme.fish
@@ -1,5 +1,4 @@
 # Defined in - @ line 2
 function playme
-	i3-msg 'move scratchpad'
-    cvlc --control dbus --random ~/Music/playme/*
+	xterm -class "playmeplayer" -e "cvlc -I rc --control dbus --random ~/Music/playme/*"
 end
