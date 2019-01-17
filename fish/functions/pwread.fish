283af0184d2fafeb4102c936d308a135f11f9d4b
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-01-17 15:37:32 +0100

    Simplify pwread

--- fish/functions/pwread.fish
+++ fish/functions/pwread.fish
@@ -1,14 +1,3 @@
-# Defined in - @ line 2
 function pwread
-	set readcmd openssl enc -d -aes-256-cbc -in ~/.pw
-    set nargs (count $argv)
-    if math "$nargs > 1" >/dev/null
-        echo "too many arguments" >&2
-        return 1
-    end
-    if math "$nargs == 1" >/dev/null
-        eval $readcmd | grep $argv[1]
-    else
-        eval $readcmd
-    end
+	openssl enc -d -aes-256-cbc -pbkdf2 -in ~/.pw
 end
