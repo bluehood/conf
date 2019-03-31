ab86fece9f22d205a184c7533a07d4d23f308f31
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-03-31 19:30:46 +0200

    Better sourceroot function

--- fish/functions/sourceroot.fish
+++ fish/functions/sourceroot.fish
@@ -1,13 +1,15 @@
+# Defined in - @ line 2
 function sourceroot
 	set nargs (count $argv)
     if [ $nargs -gt 1 ]
         echo "too many arguments" >&2
         return 1
+    else if [ $nargs -eq 1 ]
+        set rootbld $argv[1]
+    else
+        set rootbld "build"
     end
-    set rootbld $argv[1]
-    set -q rootbld[1]
-    or set rootbld "root_build"
-    set thisroot "~/ROOT/$rootbld/bin/thisroot.sh"
+    set thisroot ~/ROOT/{$rootbld}/bin/thisroot.fish
     echo "sourcing $thisroot"
-    bash -c "source $thisroot && fish"
+    source {$thisroot}
 end
