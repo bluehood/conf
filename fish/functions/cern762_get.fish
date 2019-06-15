54670cc3a6ef0eaaed2c9ae7363510eb93bb06ea
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-06-15 16:10:02 +0200

    [FISH] Use -f instaed of -rf in cern762_get.fish

--- fish/functions/cern762_get.fish
+++ fish/functions/cern762_get.fish
@@ -4,5 +4,5 @@ function cern762_get
     echo "copying \"$fname\" from cern762..."
     ssh eguiraud@lxplus.cern.ch -- scp -r eguiraud@cern762:$argv[1] blue@lcm.mi.infn.it:.
     and scp -r blue@lcm.mi.infn.it:$fname .
-    and ssh blue@lcm.mi.infn.it -- rm -rf $fname
+    and ssh blue@lcm.mi.infn.it -- rm -f $fname
 end
