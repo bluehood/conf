83dfbe430e3923d27282ba308b5f80a2f43447b7
Enrico Guiraud <enrico.guiraud@cern.ch>
2019-12-13 10:01:21 +0100

    [FISH] Improvements to cern762_get

--- fish/functions/cern762_get.fish
+++ fish/functions/cern762_get.fish
@@ -1,8 +1,8 @@
-# Defined in - @ line 2
+# Defined in /tmp/fish.ful4Ce/cern762_get.fish @ line 2
 function cern762_get
-	set fname (basename $argv[1])
-    echo "copying \"$fname\" from cern762..."
-    ssh eguiraud@lxplus.cern.ch -- scp -r eguiraud@cern762:$argv[1] blue@lcm.mi.infn.it:.
-    and scp -r blue@lcm.mi.infn.it:$fname .
-    and ssh blue@lcm.mi.infn.it -- rm -f $fname
+   set fname (basename $argv[1])
+   echo "copying \"$fname\" from cern762..."
+   ssh eguiraud@lxplus.cern.ch -- scp -r eguiraud@cern762:$argv[1] blue@lcm.mi.infn.it:scratchspace/.
+   and scp -r blue@lcm.mi.infn.it:scratchspace/$fname .
+   and ssh blue@lcm.mi.infn.it -- rm -rf scratchspace/$fname
 end
