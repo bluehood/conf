218e3d6ea6aa12d89e4b2f33223ee8442bab55b0
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-06-22 13:16:38 +0200

    [FISH] Minor improvements to ytstop

--- fish/functions/ytstop.fish
+++ fish/functions/ytstop.fish
@@ -1,6 +1,7 @@
-# Defined in /tmp/fish.Fbixig/ytstop.fish @ line 2
+# Defined in /tmp/fish.pR6ECU/ytstop.fish @ line 2
 function ytstop
    # grep [y]tdl to exclude grep itself from the grepping
-   set MPV_PID (ps xo pid,command --no-headers | grep -m1 "mpv .* [y]tdl" | cut -f2 -d' ')
+   set MPV_PID (ps xo pid,command --no-headers | grep -m1 "mpv .* [y]tdl" | cut -f3 -d' ')
    test -n "$MPV_PID" && kill $MPV_PID
+   return 0
 end
