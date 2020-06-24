5a1dea697dd923a50b39d8660b7703c6666b3461
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-06-24 20:13:30 +0200

    [FISH] Better vr function to open files from a vim terminal

--- fish/functions/vr.fish
+++ fish/functions/vr.fish
@@ -1,4 +1,9 @@
-# Defined in /tmp/fish.9QrjN5/vr.fish @ line 1
+# Defined in /tmp/fish.urpUCr/vr.fish @ line 2
 function vr
-   vim --servername $VIM_SERVERNAME --remote $argv
+   vim --servername $VIM_SERVERNAME --remote-send "<C-S>:cd $PWD<CR>"
+   if test (count $argv) -eq 0
+      vim --servername $VIM_SERVERNAME --remote-send '<C-S>N:enew<CR>'
+   else
+      vim --servername $VIM_SERVERNAME --remote $argv
+   end
 end
