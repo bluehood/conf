0b65dc541bcba066d52adaee725dc437b4e6c751
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-06-26 12:20:36 +0200

    [FISH] Let `vr` open documents in new tabs

--- fish/functions/vr.fish
+++ fish/functions/vr.fish
@@ -1,9 +1,9 @@
-# Defined in /tmp/fish.urpUCr/vr.fish @ line 2
+# Defined in /tmp/fish.J4pn09/vr.fish @ line 2
 function vr
    vim --servername $VIM_SERVERNAME --remote-send "<C-S>:cd $PWD<CR>"
    if test (count $argv) -eq 0
-      vim --servername $VIM_SERVERNAME --remote-send '<C-S>N:enew<CR>'
+      vim --servername $VIM_SERVERNAME --remote-send '<C-S>N:tabe<CR>'
    else
-      vim --servername $VIM_SERVERNAME --remote $argv
+      vim --servername $VIM_SERVERNAME --remote-tab $argv
    end
 end
