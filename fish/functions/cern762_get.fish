942fc15432eb544e11078edce6f3f0f9d8837354
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-07-24 13:13:04 +0200

    Use lxplus rather than LCM as middle hop in cern762_get

--- fish/functions/cern762_get.fish
+++ fish/functions/cern762_get.fish
@@ -1,8 +1,8 @@
-# Defined in /tmp/fish.ful4Ce/cern762_get.fish @ line 2
+# Defined in /tmp/fish.1oEmnT/cern762_get.fish @ line 2
 function cern762_get
    set fname (basename $argv[1])
    echo "copying \"$fname\" from cern762..."
-   ssh eguiraud@lxplus.cern.ch -- scp -r eguiraud@cern762:$argv[1] blue@lcm.mi.infn.it:scratchspace/.
-   and scp -r blue@lcm.mi.infn.it:scratchspace/$fname .
-   and ssh blue@lcm.mi.infn.it -- rm -rf scratchspace/$fname
+   ssh eguiraud@lxplus.cern.ch -- scp -r eguiraud@cern762:$argv[1] /eos/user/e/eguiraud/scratch/.
+   and scp -r eguiraud@lxplus.cern.ch:/eos/user/e/eguiraud/scratch/$fname .
+   and ssh eguiraud@lxplus.cern.ch -- rm -rf /eos/user/e/eguiraud/scratch/$fname
 end
