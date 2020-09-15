428ff9c54c61707a64ddfbeae005a02237add0eb
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-09-15 23:25:17 +0200

    [FISH] Mover vr -> v

--- fish/functions/v.fish
+++ fish/functions/v.fish
@@ -1,5 +1,5 @@
-# Defined in /tmp/fish.J4pn09/vr.fish @ line 2
-function vr
+# Defined in /tmp/fish.xMkIaR/vr.fish @ line 2
+function v
    vim --servername $VIM_SERVERNAME --remote-send "<C-S>:cd $PWD<CR>"
    if test (count $argv) -eq 0
       vim --servername $VIM_SERVERNAME --remote-send '<C-S>N:tabe<CR>'
