b9bcd0b850fecbca274efcfd95c5777a699082e6
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-09-30 11:37:03 +0200

    [FISH] Remove v function, make v an abbr for vim

--- fish/functions/v.fish
+++
@@ -1,9 +0,0 @@
-# Defined in /tmp/fish.xMkIaR/vr.fish @ line 2
-function v
-   vim --servername $VIM_SERVERNAME --remote-send "<C-S>:cd $PWD<CR>"
-   if test (count $argv) -eq 0
-      vim --servername $VIM_SERVERNAME --remote-send '<C-S>N:tabe<CR>'
-   else
-      vim --servername $VIM_SERVERNAME --remote-tab $argv
-   end
-end
